9b9746fcee9e8ef1ffd2518a5af4a19d843051a3
unknown <luccasdrapala@gmail.com>
2022-09-04 10:51:37 -0300

    instalando composer.phar

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'miniframework/php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '57f270d372bb4449aec7aabc7ed8d9588bafbdba',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'miniframework/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '57f270d372bb4449aec7aabc7ed8d9588bafbdba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
