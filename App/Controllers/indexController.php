04b31229e130aac64335c313eb41757fc2c58c1b
unknown <luccasdrapala@gmail.com>
2022-09-22 21:27:01 -0300

    implementando metodo render() e abstraindo o mesmo

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -5,11 +5,24 @@ namespace App\Controllers;
 class IndexController {
 
     public function index() {
-        require_once "../App/Views/index/index.phtml";
+        $this->render('index');
     }
 
     public function sobreNos() {
-        require_once "../App/Views/index/sobreNos.phtml";
+        $this->render('sobreNos');
+    }
+
+    public function render($view) {
+        //echo(get_class($this)); retorna o local da classe "App/.../..."
+
+        $classAtual = get_class($this);
+        $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
+
+        $classAtual = strtolower(str_replace('Controller', '', $classAtual)); 
+        // usa o padrão do controller para setar a pasta que esta dentro da pasta Views
+        
+        //forma dinamico de acesso a determinada view
+        require_once "../App/Views/". $classAtual ."/" .$view.".phtml";    
     }
 }
 
